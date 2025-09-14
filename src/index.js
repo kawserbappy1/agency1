2c55dfe2b8078b58188a3dffcd4e5b55a82c5104
kawser-hamid-bappy <bappydu2015@gmail.com>
2025-09-14 10:53:07 +0600

    everything done

--- src/index.js
+++ src/index.js
@@ -35,3 +35,53 @@ var swiper = new Swiper(".reviews", {
     dynamicBullets: true,
   },
 });
+
+// scroll up
+const scroppUp = () => {
+  const scrollUpBtn = document.getElementById("scrollUpBtn");
+  if (scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2");
+    scrollUpBtn.classList.add("bottom-10");
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2");
+    scrollUpBtn.classList.remove("bottom-10");
+  }
+};
+window.addEventListener("scroll", scroppUp);
+
+// active link
+const activeLink = () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav_link");
+  let current = "home";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (this.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    if (link.href.includes(current)) {
+      link.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", activeLink);
+
+// Change header
+const scrollHeader = () => {
+  const navBar = document.getElementById("navBar");
+
+  if (scrollY >= 50) {
+    navBar.classList.add("bgHeader");
+  } else {
+    navBar.classList.remove("bgHeader");
+  }
+};
+
+window.addEventListener("scroll", scrollHeader);
